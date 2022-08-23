c775c798798c089b24ccfb1df7e3d2d3d087b7f8
Dawit Asmero <76691105+dawitasmeroatinafu@users.noreply.github.com>
2022-08-23 21:05:55 +0300

    creating scripts for cleaning

---
+++ scripts/missing_percentage.py
@@ -0,0 +1,16 @@
+import numpy as np
+#calculate the missing percent by each 
+
+def percent_missing(df):
+
+    # Calculate total number of cells in dataframe
+    totalCells = np.product(df.shape)
+
+    # Count number of missing values per column
+    missingCount = df.isnull().sum()
+
+    # Calculate total number of missing values
+    totalMissing = missingCount.sum()
+
+    # Calculate percentage of missing values
+    print("The telecom dataset contains", round(((totalMissing/totalCells) * 100), 2), "%", "missing values.")
