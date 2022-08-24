197373447787dfc313e9a4628c11f649f9277392
Dawit Asmero <76691105+dawitasmeroatinafu@users.noreply.github.com>
2022-08-24 09:15:06 +0300

    data summerzer class

--- scripts/data_summry.py
+++ scripts/data_summry.py
@@ -1,16 +1,20 @@
-def summ_columns(self, df, unique=True):
-    """
-    shows columns and their missing values along with data types.
-    """
-    df2 = df.isna().sum().to_frame().reset_index()
-    df2.rename(columns = {'index':'variables', 0:'missing_count'}, inplace = True)
-    df2['missing_percent_(%)'] = round(df2['missing_count']*100/df.shape[0])
-    data_type_lis = df.dtypes.to_frame().reset_index()
-    df2['data_type'] = data_type_lis.iloc[:,1]
-    
-    if(unique):
-        unique_val = []
-        for i in range(df2.shape[0]):
-            unique_val.append(len(pd.unique(df[df2.iloc[i,0]])))
-        df2['unique_values'] = pd.Series(unique_val)
-    return df2
+import pandas as pd
+import numpy as np
+class summery:
+
+    def summ_columns(self, df, unique=True):
+        """
+        shows columns and their missing values along with data types.
+        """
+        df2 = df.isna().sum().to_frame().reset_index()
+        df2.rename(columns = {'index':'variables', 0:'missing_count'}, inplace = True)
+        df2['missing_percent_(%)'] = round(df2['missing_count']*100/df.shape[0])
+        data_type_lis = df.dtypes.to_frame().reset_index()
+        df2['data_type'] = data_type_lis.iloc[:,1]
+        
+        if(unique):
+            unique_val = []
+            for i in range(df2.shape[0]):
+                unique_val.append(len(pd.unique(df[df2.iloc[i,0]])))
+            df2['unique_values'] = pd.Series(unique_val)
+        return df2
