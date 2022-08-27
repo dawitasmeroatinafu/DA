9620972652153c740e574393ec2d7f34adb93204
Dawit Asmero <76691105+dawitasmeroatinafu@users.noreply.github.com>
2022-08-27 19:47:58 +0300

    building sashboard

--- models/multiapp.py
+++ models/multiapp.py
@@ -1,54 +1,48 @@
-import pandas as pd
-import numpy
+"""Frameworks for running multiple Streamlit applications as a single app.
+"""
 import streamlit as st
-import os
-import pickle
-from sklearn.preprocessing import StandardScaler
-from PIL import Image
-# setting path to file and folders
-user_df= pd.read_csv("./data/teleco_user_sat_data.csv")
 
-st.title("What is your customer's satisfaction level?")
-st.subheader("This model will predict the satisfaction score of a user")
+class MultiApp:
+    """Framework for combining multiple streamlit applications.
+    Usage:
+        def foo():
+            st.title("Hello Foo")
+        def bar():
+            st.title("Hello Bar")
+        app = MultiApp()
+        app.add_app("Foo", foo)
+        app.add_app("Bar", bar)
+        app.run()
+    It is also possible keep each application in a separate file.
+        import foo
+        import bar
+        app = MultiApp()
+        app.add_app("Foo", foo.app)
+        app.add_app("Bar", bar.app)
+        app.run()
+    """
+    def __init__(self):
+        self.apps = []
 
-model = pickle.load(open("./models/pridict_satisfaction_model.sav", 'rb'))
-#result = loaded_model.score(x_test, y_test)
-#result
+    def add_app(self, title, func):
+        """Adds a new application.
+        Parameters
+        ----------
+        func:
+            the python function to render this app.
+        title:
+            title of the app. Appears in the dropdown in the sidebar.
+        """
+        self.apps.append({
+            "title": title,
+            "function": func
+        })
 
-xdr = st.number_input("xDr_session_count")
-tot = st.number_input("Total Data Volume")
-dur = st.number_input("Total Session Duration")
-rtt = st.number_input("Average RTT")
-tp = st.number_input("Average Throughput")
-tcp = st.number_input("TCP volume")
+    def run(self):
+        # app = st.sidebar.radio(
+        app = st.sidebar.selectbox(
+            'Navigation',
+            self.apps,
+            format_func=lambda app: app['title'])
 
-input_data = [xdr , tot, dur , rtt, tp, tcp]
-prediction = model.predict([input_data])
-
-st.subheader("The satisfaction score is: {}".format ( prediction))
-
-st.subheader("The Dataset")
-
-st.write(
-  user_df
-)
-
-st.subheader("Applications Data Usage")
-image = Image.open('./image/applications.jpg')
-st.image(image, caption='scatter plot of applications and total data volume')
-
-st.subheader("Top 3 Handset Manufacturers")
-image2 = Image.open('./image/top3.png')
-st.image(image2, caption='Top 3 Handset Manufacturers')
-
-st.subheader("Top 10 Handset Types")
-image3 = Image.open('./image/top10.png')
-st.image(image3, caption='Top 10 Handset Types')
-
-st.subheader("Engagement metrics Distribution")
-image4 = Image.open('./image/totdist.png')
-st.image(image4, caption='Distribution of engagement matrics: xdr, total data volume, session duration')
-
-st.subheader("Experience metrics Distribution")
-image5 = Image.open('./image/rttdist.png')
-st.image(image5, caption='Distribution of experience matrics: RTT, TP, TCP')
+        app['function']()
