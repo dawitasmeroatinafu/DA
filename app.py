106853efc6e0b8348e9636876134e7a4faffed15
Dawit Asmero <76691105+dawitasmeroatinafu@users.noreply.github.com>
2022-08-28 21:00:37 +0300

    creating visual dashboard with streamlit

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from multiapp import MultiApp
 from PIL import Image
 from images import * # import your app modules here
 
-app = MultiApp()
-
 st.sidebar.markdown('# **Telecom Bussiness Analysis**')
 st.sidebar.markdown("""
 Before investing on a business it is a must to have best understanding about the field. This project is all about analyzing TellCo's user and find out whether it is worth buying or selling.
@@ -17,13 +15,19 @@ submitted=st.button('submit')
 if(select_events=='user_overview analysis'):
     st.subheader("top 10 handsets")
     image=Image.open('./images/top 10 handset.png')
+    st.image(image, caption='')
     st.subheader("top 3 hand set manufacturer")
     image=Image.open('./images/handset manufacturer.PNG')
+    st.image(image, caption='')
+
 
 if(select_events=='user_engagement analysis'):
     st.subheader("top 3 apps customers engage with.PNG")
     image=Image.open('./images/top 3 apps customers engage with.PNG')
+    st.image(image, caption='')
+
     st.subheader("maximum custermer engagement among 3 clusters")
     image=Image.open('./images/maximum custermer engagement among 3 clusters.PNG')
+    st.image(image, caption='')
 
-app.run()
+
